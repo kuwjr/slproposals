03c4a1280e3272e9807c330015a21970eb634866
kuwjr <kuwjr@outlook.com>
2022-08-13 16:45:21 +0530

    meow

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,7 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser");
-const cors = require('cors');
 
-app.use(cors({
-    origin: 'http://localhost:3000', // todo: change to env variable
-    credentials: true,
-    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
-    // allowedHeaders: ['Content-Type'],
-    // exposedHeaders: ['Content-Type']
-}));
-// app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
