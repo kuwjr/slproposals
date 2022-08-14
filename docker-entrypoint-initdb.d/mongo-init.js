5c631dd46c56e5060c4b832a5ea56e3f99bf838f
kuwjr <kuwjr@outlook.com>
2022-08-14 08:42:10 +0530

    fix: mongodb authentication error

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -4,7 +4,7 @@ db.createUser(
         pwd: "meow",
         roles: [
             {
-                role: "userAdminAnyDatabase",
+                role: "readWrite",
                 db: "slproposals"
             }
         ]
