c4c0796261f86d948579b4efa8db28d21b7a7018
kuwjr <kuwjr@outlook.com>
2022-08-23 21:52:18 +0530

    Fix: lol

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000'
+axios.defaults.baseURL = 'http://localhost:5000'
 axios.defaults.withCredentials = true
 
 export default axios
