0077291dc71290b23240a4ce06e6702d51010d8f
kuwjr <kuwjr@outlook.com>
2022-08-27 16:48:31 +0530

    mmm

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://api.slproposals.lk'
+axios.defaults.baseURL = 'http://0.0.0.0:5000'
 axios.defaults.withCredentials = true
 
 export default axios
